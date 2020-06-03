8bddb54c739228ee5afd5a4cee2d76c2fce46c76
BuleandraGeorge <buleandrageorge@gmail.com>
2020-06-03 19:21:18 +0000

    Connected to the db

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cook_idea_db'
+app.config["MONGO_URI"] = 'mongodb+srv://chief_user:cookpass@cookbook-zwtc4.mongodb.net/cook_idea_db?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def text():
-    return "Cook ideas comming soon"
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('receipes_list.html', receipes=mongo.db.receipes.find())
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
