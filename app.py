5e1159198ad4427713526233114f2b973a371991
BuleandraGeorge <buleandrageorge@gmail.com>
2020-06-14 19:50:13 +0000

    Created recipe_details_modal.html

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
+def update_lists(the_list, new_elements):
+        exist = 1
+        for x in the_list:
+            for y in new_elements:
+                if x != y:
+                    the_list.append(y)
+                    exist = 0
+        the_list.sort()
+        return [the_list, exist]
+
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cook_idea_db'
@@ -17,13 +28,19 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')
 def get_recipes():
     return render_template('recipes_list.html', recipes=mongo.db.recipes.find(),
-     zones=mongo.db.country.find(), types=mongo.db.type.find())
+    zones=mongo.db.country.find(), types=mongo.db.type.find())
     
 
 @app.route("/find_recipes")
 def display_recipes():
     name = request.form.get('search-name')
     return render_template('recipes_list.html', recipes=mongo.db.recipes.find({name: name}))
+
+
+@app.route('/#modal1/<rep_id')
+def modal():
+    recipe = mongo.db.recipes.find_one('_id': ObjectId(rep_id))
+    return render_template('recipe_details_modal.html', recipe=recipe)
 
 
 @app.route('/edit_recipe.html/<rep_id>')
@@ -33,9 +50,60 @@ def edit(rep_id):
     return render_template('edit_recipe.html', recipe=recipe)
 
 
-@app.route('/upload')
-def upload():
+@app.route('/upload/<rep_id>', methods=["POST", "GET"])
+def upload(rep_id):
+    recipes = mongo.db.recipes
+    countries = mongo.db.country
+    types = mongo.db.dish_types
+    if request.method == "POST":
+        recipe_name = request.form.get("rep_name")
+        recipe_country = request.form.getlist("rep_country")
+        recipe_type = request.form.getlist("rep_type")
+        recipe_vegan = request.form.get("rep_vegan")
+        recipe_description = request.form.get("rep_description")
+        recipe_url = request.form.get("rep_url")
+        recipe_tools = request.form.getlist("rep_tool")
+        recipe_igd = request.form.getlist("rep_igd")
+        recipe_steps = request.form.getlist("rep_step")
+        recipe_author = request.form.get("rep_author")
+        recipe_source = request.form.get('rep_source')
+        time = datetime.now()
+        date = time.strftime("%d/%m/%Y")
+        recipes.update_one({"_id": ObjectId(rep_id)},
+                    { 
+                        '$set':{
+                        "name": recipe_name,
+                        "description": recipe_description,
+                        "tools": recipe_tools,
+                        "ingredients": recipe_igd,
+                        "steps": recipe_steps,
+                        "country": ('any', recipe_country),
+                        "types": ('any',recipe_type),
+                        "vegan": recipe_vegan,
+                        "rating": ["0", "0"],
+                        "url": recipe_url,
+                        "author": recipe_author,
+                        "upload_date": date,
+                        "source": recipe_source
+                        }
+                 })
+        new_list = update_lists(countries.zones, recipe_country)
+        if new_list[1] == 0:
+            countries.update_one({'_id': ObjectId(countries._id)},
+                        {'$set': {
+                                'zone' : new_list[0]
+                                }
+                        }) 
 
+        new_list = update_lists(types.rep_type, recipe_type)
+        if new_list[1] == 0:
+            types.update_one({'_id': ObjectId(types._id)},
+                    {'$set': {
+                                'rep_type': new_list[0]
+                            }
+                    })
+
+        return redirect(url_for('get_recipes'))
 
 
 @app.route('/like/<rep_id>')
@@ -71,61 +139,53 @@ def dislike(rep_id):
 def add():
     countries = mongo.db.country
     types = mongo.db.dish_types
-    def update_lists(the_list, new_elements):
-        exist = 1
-        for x in the_list:
-            for y in new_elements:
-                if x != y:
-                    the_list.append(y)
-                    exist = 0
-        the_list.sort()
-        return [the_list, exist]
-
     if request.method == "POST":
-                recipe_name = request.form.get("rep_name")
-                recipe_country = request.form.getlist("rep_country")
-                recipe_type = request.form.getlist("rep_type")
-                recipe_vegan = request.form.get("rep_vegan")
-                recipe_description = request.form.get("rep_description")
-                recipe_url = request.form.get("rep_url")
-                recipe_tools = request.form.getlist("rep_tool")
-                recipe_igd = request.form.getlist("rep_igd")
-                recipe_steps = request.form.getlist("rep_step")
-                recipe_author = request.form.get("rep_author")
-                recipe_source = request.form.get('rep_source')
-                time = datetime.now()
-                date = time.strftime("%d/%m/%Y")
-                new_recipe= {
+        recipe_name = request.form.get("rep_name")
+        recipe_country = request.form.getlist("rep_country")
+        recipe_type = request.form.getlist("rep_type")
+        recipe_vegan = request.form.get("rep_vegan")
+        recipe_description = request.form.get("rep_description")
+        recipe_url = request.form.get("rep_url")
+        recipe_tools = request.form.getlist("rep_tool")
+        recipe_igd = request.form.getlist("rep_igd")
+        recipe_steps = request.form.getlist("rep_step")
+        recipe_author = request.form.get("rep_author")
+        recipe_source = request.form.get('rep_source')
+        time = datetime.now()
+        date = time.strftime("%d/%m/%Y")
+        new_recipe= {
                     "name" : recipe_name,
                     "description": recipe_description,
-                    "tools":(recipe_tools),
-                    "ingredients": (recipe_igd),
-                    "steps":(recipe_steps),
-                    "country" :('any',recipe_country),
-                    "types":('any',recipe_type),
-                    "vegan":recipe_vegan,
-                    "rating":["0","0"],
-                    "url":recipe_url,
-                    "author":recipe_author,
-                    "upload_date":date,
-                    "source":recipe_source
+                    "tools": recipe_tools,
+                    "ingredients": recipe_igd,
+                    "steps": recipe_steps,
+                    "country": ('any', recipe_country),
+                    "types": ('any', recipe_type),
+                    "vegan": recipe_vegan,
+                    "rating": ["0", "0"],
+                    "url": recipe_url,
+                    "author": recipe_author,
+                    "upload_date": date,
+                    "source": recipe_source
                 }
-                recipes = mongo.db.recipes
-                recipes.insert_one(new_recipe)
-                new_list = update_lists(countries.zones, recipe_country)
-                if new_list[1] == 0:
-                    countries.update_one({'_id': ObjectId(countries._id)},
-                            {'$set': {
-                                        'zone' : new_list[0]
-                                    }
-                            }) 
+        recipes = mongo.db.recipes
+        recipes.insert_one(new_recipe)
+        new_list = update_lists(countries.zones, recipe_country)
+        if new_list[1] == 0:
+            countries.update_one({'_id': ObjectId(countries._id)},
+                    {'$set': {
+                             'zone' : new_list[0]
+                            }
+                    }) 
                 
-                new_list = update_lists(types.rep_type, recipe_type)
-                if new_list[1] == 0:
-                   types.update_one({'_id': ObjectId(types._id)},
-                            {'$set': {
-                                        'rep_type' : new_list[0]
-                            })
+        new_list = update_lists(types.rep_type, recipe_type)
+        if new_list[1] == 0:
+            types.update_one({'_id': ObjectId(types._id)},
+                    {'$set': {
+                        'rep_type' : new_list[0]
+                            }
+                    })
+                            
     return redirect(url_for('get_recipes'))
 
 
