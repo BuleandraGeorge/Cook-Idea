4dacb6e918502634101c77b15bea23ad1a500f82
BuleandraGeorge <buleandrageorge@gmail.com>
2020-06-27 11:34:27 +0000

    Change and hide password

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def lowerList(the_list):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cook_idea_db'
-app.config["MONGO_URI"] = "mongodb+srv://chief_user:chiefpassword@cookbook-zwtc4.mongodb.net/cook_idea_db?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 
 mongo = PyMongo(app)
